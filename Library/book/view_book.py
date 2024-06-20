10d1a5f76a69ef600ee7ad39a94da85019c31d3e
SONDES MESSAI PRO <sondesmessai135@gmail.com>
2024-06-20 11:07:45 +0200

    books

--- Library/book/view_book.py
+++ Library/book/view_book.py
@@ -1,7 +1,14 @@
-from datetime import datetime
-
 
 from django.shortcuts import render
-
+from Library.catalog.models import Author
+from models import Book, State
 def index(request):
-    return render(request, 'catalog/index.html', {'date': datetime.now(), 'pseudo': 'Olive & Tom'})
+    num_books = Book.objects.all().count()
+    num_authors = Author.objects.all().count()
+    num_book_reserved =State.object.filter(status="r").count()
+    return render(request, 'catalog/books.html',
+                  context={
+                      'num_books': num_books,
+                      'num_author': num_authors,       
+                      
+                  },
--- Library/catalog/urls.py
+++ Library/catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from.import views_books
+from . import views_books
+
 urlpatterns = [
     path('', views.index, name='index'),
-      path('books',views_book.index, name='index'),
+    path('books', views_books.index, name='books'),
 ]
